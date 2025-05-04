84f604b75727e8331df6f0337ae4a7aacf2015d5
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-05-04 23:35:14 +0530

    speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './index.css';
 
 const rootElement = document.getElementById("root");
@@ -10,5 +11,6 @@ if (!rootElement) throw new Error('Failed to find the root element');
 createRoot(rootElement).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights/>
   </React.StrictMode>
 );
